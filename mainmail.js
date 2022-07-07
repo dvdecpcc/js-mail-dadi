5055d25ed48e091ebffa4e48709ed219fc9b1d23
davide <dacapoccia@gmail.com>
2022-07-07 17:07:03 +0200

    email

--- mainmail.js
+++ mainmail.js
@@ -1,4 +1,9 @@
 const emails = ['io@io.io', 'me@me.me', 'altro@altro.altro'];
 let usermail = prompt('Inserisci la tua email');
 
-console.log(usermail in emails);
+if (usermail == emails[0] || usermail == emails[1] || usermail == emails[2]){
+    alert('email corretta');
+}
+else{
+    alert('email non valida');
+}
